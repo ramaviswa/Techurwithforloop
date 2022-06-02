bddc4b06273b48020d2058e187f99e0dd9eb6407
jagadish R <viswa4323@gmail.com>
2022-06-02 12:55:57 +0530

    vnet

--- infra/resources/vnet/viswavnet.tf
+++ infra/resources/vnet/viswavnet.tf
@@ -1,3 +1,7 @@
-variable "virtualnetwork" {}
-variable "resourcegroup" {}
-variable "location" {}
+resource "azurerm_virtual_network" "viswavnet" {
+    for_each = var.virtualnetwork
+    name = each.value.vnetname
+    resource_group_name = var.resourcegroup[each.value.rgkey].name
+    location = var.location
+    address_space = each.value.address
+}
