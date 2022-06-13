10f23fc59683f24ea227e191df91add7aba2d428
jagadish R <viswa4323@gmail.com>
2022-06-13 22:46:59 +0530

    roleassignforloop

--- infra/resources/roleassignment/role.tf
+++ infra/resources/roleassignment/role.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_assignment" "roleassignmenttorg" {
     for_each = var.role.definition_name
-    role_definition_name = each.role.definition_name
+    role_definition_name = each.value.role_definition_name
     scope = var.rg[each.value.rgkey].id
     principal_id = var.users[each.value.userkey].objet_id
   
